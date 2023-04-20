ea586d038b93e4554f1242659ceaa25f66e36420
H <127960702+Edine531@users.noreply.github.com>
2023-04-20 09:35:59 +0200

    Update inserRegister.php

--- voiture/inserRegister.php
+++ voiture/inserRegister.php
@@ -3,11 +3,12 @@
 $nom = $_POST['nom'];
 $prenom = $_POST['prenom'];
 $email = $_POST['email'];
+$password = $_POST['password'];
 // Etablir la connexion vers la base de données
 require_once("connexion.php");
 // préparer la requete
 $ps = $pdo->prepare("INSERT INTO user VALUES (?, ?, ?, ? )");
-$params = array($nom, $prenom, $email);
+$params = array($nom, $prenom, $email, $password);
 $ps->execute($params);
 // si l'utilisateur existe, on va lancer la session et puis on va sauvgarder lutilisateur
 // dans la session en utilisant la collection $_SESSION
