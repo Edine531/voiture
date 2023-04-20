12277fcd7f128b8b8ecb88e541d73d66053e8c7c
Aly <87067002+Alydiabira@users.noreply.github.com>
2023-04-20 14:49:58 +0200

    Create footer.php

---
+++ voiture/footer.php
@@ -0,0 +1,29 @@
+<footer class="text-center text-white" style="background-color: light;">
+    <!-- Grid container -->
+    <div class="container pt-4">
+        <p class="text-center text-white">Rejoignez-moi sur mes réseaux</p>
+
+        <!-- Section: Social media -->
+        <section class="mb-4">
+            <!-- Facebook -->
+            <a class="btn btn-link btn-floating btn-lg text-dark m-1" href="#"
+                role="button" data-mdb-ripple-color="dark"><i class="fab fa-facebook-f text-white">
+                </i></a>
+            <!-- Linkedin -->
+            <a class="btn btn-link btn-floating btn-lg text-dark m-1" href="#"
+                role="button" data-mdb-ripple-color="dark"><i class="fab fa-linkedin text-white"></i></a>
+            <!-- Github -->
+            <a class="btn btn-link btn-floating btn-lg text-dark m-1"
+                href="https://github.com/Edine531/voiture" role="button"
+                data-mdb-ripple-color="dark"><i class="fab fa-github text-white"></i></a>
+        </section>
+        <!-- Section: Social media -->
+    </div>
+    <!-- Grid container -->
+    <!-- Copyright -->
+    <div class="text-center text-white p-3" style="background-color: #009AAE;">
+        © 2023 Copyright:
+        <a class="text-white" href="#">k-rten</a>
+    </div>
+    <!-- Copyright -->
+</footer>
