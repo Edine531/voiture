ceec7964be8674d34d7174ea55afe5efda6289b1
Aly <87067002+Alydiabira@users.noreply.github.com>
2023-04-20 14:49:56 +0200

    Update entete.php

--- voiture/entete.php
+++ voiture/entete.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg bg-dark navbar-dark fixed-top ">
+<!-- <nav class="navbar navbar-expand-lg bg-dark navbar-dark fixed-top ">
     <ul class="navbar-nav">
 
         <li class="nav-item " <?php if (isset($_SESSION['PROFILE'])) { ?>>
@@ -20,4 +20,75 @@
 <?php } ?>
 
     </ul>
+</nav>
+ -->
+
+<head>
+    <meta charset="utf-8" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+    <link rel="stylesheet" type="text/css" href="CSS/style.css" />
+    <!-- Font Awesome -->
+<link
+  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
+  rel="stylesheet"
+/>
+<!-- Google Fonts -->
+<link
+  href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+  rel="stylesheet"
+/>
+<!-- MDB -->
+<link
+  href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.2.0/mdb.min.css"
+  rel="stylesheet"
+/>
+</head>
+
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item " <?php if (isset($_SESSION['PROFILE'])) { ?>>
+                        <a class="nav-link" href="locataire.php">Etudiants</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="saisieLocataire.php">Saisie</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="logOut.php">Se déconnecter</a>
+                    </li>
+                <?php } ?>
+                    <li class="nav-item" <?php if (!isset($_SESSION['PROFILE'])) { ?>>
+                        <a class="nav-link" href="login.php">Se connecter </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="register.php">s'inscrire </a>
+                    </li>
+                <?php } ?>      
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Dropdown
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Action</a></li>
+                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                        <li><hr class="dropdown-divider"></li>
+                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    </ul>
+                    </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled">Disabled</a>
+                </li>
+            </ul>
+            <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+        </div>
+    </div>
 </nav>
